2bf43abad69373b722bf4e133c83bd5f12f4a2d2
gabrielramost <83712300+gabrielramost@users.noreply.github.com>
2026-04-20 16:21:32 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+const items = document.querySelectorAll(".timeline-item");
 
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add("show");
+    }
+  });
+},{ threshold:0.2 });
+
+items.forEach(item=>{
+  observer.observe(item);
+});
