1d2050b4feb500fb862622ed60fe7d66cf60b7d9
gabrielramost <83712300+gabrielramost@users.noreply.github.com>
2026-04-25 23:43:44 -0500

    Create guests.js

---
+++ js/guests.js
@@ -0,0 +1,17 @@
+const invitados = [
+  {
+    nombre: "Gabriel Ramos",
+    invitados: 2,
+    acompanante: "Akemi"
+  },
+  {
+    nombre: "Juan Pérez",
+    invitados: 1,
+    acompanante: null
+  },
+  {
+    nombre: "María López",
+    invitados: 2,
+    acompanante: "Carlos"
+  }
+];
