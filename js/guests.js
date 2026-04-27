d277dd42f19be34fee0e845fad1038f32b559c43
gabrielramost <83712300+gabrielramost@users.noreply.github.com>
2026-04-26 23:51:18 -0500

    Update guests.js

--- js/guests.js
+++ js/guests.js
@@ -1,14 +1,113 @@
 const invitados = [
-  {
-    nombre: "Gabriel Ramos",
-    pases: 5,
-    acompanantes: ["Akemi Toma", "Diana Toma", "Ignacio Ramos", "Maria Timana"]
-  },
-  {
-    nombre: "Juan Pérez",
-    pases: 2,
-    acompanantes: ["Mariela Pérez", "Erick Antunez"]
-  }
+
+{ nombre: "Henry Monzón", pases: 2, acompanantes: ["Diana Toma"] },
+{ nombre: "Stephanie Monzón", pases: 2, acompanantes: ["Balbina Adrianzen"] },
+{ nombre: "Josselyn Ternero", pases: 2, acompanantes: ["Esmeralda Monzón"] },
+{ nombre: "Alonso Monzón", pases: 1, acompanantes: [] },
+
+{ nombre: "Jenny Toma", pases: 4, acompanantes: ["Edwin Lezama", "María José Lezama", "Mery Toma"] },
+{ nombre: "Tani Lezama", pases: 2, acompanantes: ["Julián"] },
+{ nombre: "Ritsuko Toma", pases: 1, acompanantes: [] },
+{ nombre: "Luis Toma", pases: 1, acompanantes: [] },
+{ nombre: "Mónica Toma", pases: 1, acompanantes: [] },
+
+{ nombre: "Julio Toma", pases: 3, acompanantes: ["Tomi Toma", "Ellen"] },
+{ nombre: "Juan Toma", pases: 3, acompanantes: ["Yolanda Suarez", "Mariana Toma"] },
+{ nombre: "Jessica Toma", pases: 4, acompanantes: ["Mario Kiyohara", "Emiliano Kiyohara", "Bárbara Kiyohara"] },
+{ nombre: "Kio Toma", pases: 1, acompanantes: [] },
+
+{ nombre: "Pedro Tejada", pases: 3, acompanantes: ["Lourdes Obregón", "Brenda Tejada"] },
+{ nombre: "Cristopher Tejada", pases: 2, acompanantes: ["Patricia Pretel"] },
+{ nombre: "Pedro André Tejada", pases: 2, acompanantes: ["Olenka"] },
+
+{ nombre: "Patricia Toma", pases: 3, acompanantes: ["Lucho Ivazeta", "Bruno Ivazeta"] },
+{ nombre: "Jose Monzón", pases: 2, acompanantes: ["Sarita"] },
+
+{ nombre: "Manuel Tsukuyama", pases: 2, acompanantes: ["Esposa"] },
+{ nombre: "Oscar Ibañez", pases: 2, acompanantes: ["Esposa"] },
+
+{ nombre: "Sra. Molly", pases: 1, acompanantes: [] },
+{ nombre: "Bertha Mendez", pases: 2, acompanantes: ["Wilder Sabino"] },
+
+{ nombre: "Ignacio Ramos", pases: 3, acompanantes: ["María Elena Timaná", "César Ramos"] },
+
+{ nombre: "Luis Salcedo", pases: 4, acompanantes: ["Urbana Ramos", "Indira Salcedo", "Keydira Salcedo"] },
+
+{ nombre: "Jorge Navarro", pases: 1, acompanantes: [] },
+
+{ nombre: "Karina Zamudio", pases: 4, acompanantes: ["Luis Román", "Irania Román", "Leandra Román"] },
+
+{ nombre: "Abad Chumbile", pases: 2, acompanantes: ["Paulina Ramos"] },
+{ nombre: "Victoria Acosta", pases: 1, acompanantes: [] },
+
+{ nombre: "Lady Chumbile", pases: 2, acompanantes: ["Esposo"] },
+{ nombre: "Janeth Chumbile", pases: 2, acompanantes: ["Esposo"] },
+
+{ nombre: "Sara Timaná", pases: 4, acompanantes: ["Robert Malca", "María Fe Malca", "Claudia Malca"] },
+
+{ nombre: "Elvia Cruz", pases: 4, acompanantes: ["Jonathan Cruz", "Cesar Timaná Yovera", "César Timaná Cruz"] },
+
+{ nombre: "César Timaná Lluncor", pases: 1, acompanantes: [] },
+
+{ nombre: "César Cochola", pases: 3, acompanantes: ["Jonny Cochola", "Roxana Timaná"] },
+
+{ nombre: "Elva Camarena", pases: 1, acompanantes: [] },
+{ nombre: "Juan Ramos", pases: 1, acompanantes: [] },
+
+{ nombre: "Maite Ramos", pases: 2, acompanantes: ["Ricardo Llaja"] },
+
+{ nombre: "Juan Antonio Silva", pases: 2, acompanantes: ["Esposa"] },
+
+{ nombre: "Patricia Balbuena", pases: 1, acompanantes: [] },
+{ nombre: "Diana Tamashiro", pases: 1, acompanantes: [] },
+
+{ nombre: "Christina Sanchez", pases: 2, acompanantes: ["Luis Mesías"] },
+
+{ nombre: "Karen Calderon", pases: 1, acompanantes: [] },
+{ nombre: "Sandra Manrique", pases: 1, acompanantes: [] },
+{ nombre: "Ivan Castillo", pases: 1, acompanantes: [] },
+
+{ nombre: "José Reyes", pases: 2, acompanantes: ["Gabriela Carrasco"] },
+
+{ nombre: "Alejandro Kluver", pases: 2, acompanantes: ["Rubi Sanchez"] },
+{ nombre: "Rodrigo Del Rio", pases: 2, acompanantes: ["Scarlett Motta"] },
+
+{ nombre: "Iván Ramírez", pases: 1, acompanantes: [] },
+{ nombre: "Omar Livia", pases: 1, acompanantes: [] },
+{ nombre: "Fiorella Canseco", pases: 1, acompanantes: [] },
+{ nombre: "Airam Bello", pases: 1, acompanantes: [] },
+{ nombre: "Joanna Fischer", pases: 1, acompanantes: [] },
+
+{ nombre: "Javier Ramírez", pases: 2, acompanantes: ["Esposa"] },
+
+{ nombre: "Claudia Guembes", pases: 1, acompanantes: [] },
+{ nombre: "Joel Odar", pases: 1, acompanantes: [] },
+{ nombre: "Camila Salazar", pases: 1, acompanantes: [] },
+{ nombre: "Erick Antunez", pases: 1, acompanantes: [] },
+{ nombre: "Richard Quispe", pases: 1, acompanantes: [] },
+{ nombre: "Ryan Privat", pases: 1, acompanantes: [] },
+{ nombre: "Luis Prieto", pases: 1, acompanantes: [] },
+{ nombre: "Vittorio Brissolese", pases: 1, acompanantes: [] },
+{ nombre: "Ana Paula Galvez", pases: 1, acompanantes: [] },
+{ nombre: "Mafer Misajel", pases: 1, acompanantes: [] },
+{ nombre: "Fernando Luque", pases: 1, acompanantes: [] },
+{ nombre: "Katia Uriol", pases: 1, acompanantes: [] },
+{ nombre: "Hanks Sarmiento", pases: 1, acompanantes: [] },
+{ nombre: "Berenice Anamaria", pases: 1, acompanantes: [] },
+{ nombre: "Jose Soto", pases: 1, acompanantes: [] },
+
+{ nombre: "Claudia García", pases: 2, acompanantes: ["Héctor Rodriguez"] },
+
+{ nombre: "Ana Peña", pases: 1, acompanantes: [] },
+
+{ nombre: "Estefani Montenegro", pases: 2, acompanantes: ["Genaro Meza"] },
+
+{ nombre: "Susana Casanova", pases: 1, acompanantes: [] },
+
+{ nombre: "Christian León", pases: 2, acompanantes: ["Sayuri Ortiz"] },
+
+{ nombre: "Renzo Anglas", pases: 1, acompanantes: [] },
+{ nombre: "Victor Gonzales", pases: 1, acompanantes: [] },
+{ nombre: "Malú Menacho", pases: 1, acompanantes: [] }
+
 ];
-
-
